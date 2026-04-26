af9bdca5b8d44f7abe8e2e68a876e52abd5f6bd5
Kwan Wu <kwanwu@users.noreply.github.com>
2026-04-26 15:48:57 -0400

    Initial static website

---
+++ script.js
@@ -0,0 +1,7 @@
+// Soda Ship Storefront JavaScript
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Soda Ship storefront loaded');
+    
+    // Add your JavaScript functionality here
+});
